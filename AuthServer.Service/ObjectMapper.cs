16ead1f11d1dad6040ac56a6127984085e7e1ff7
uğur cengiz <ugur_nobre@hotmail.com>
2022-08-10 16:18:27 +0300

    10.08.2022

---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+
+            return config.CreateMapper();
+
+        });
+
+        public static IMapper Mapper => lazy.Value;
+
+    }
+}
--- IdentityAuthServer.API/Startup.cs
+++ IdentityAuthServer.API/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthServer.Core.Configuration;
+using SharedLibrary.Configurations;
 
 namespace IdentityAuthServer.API
 {
@@ -26,6 +28,9 @@ namespace IdentityAuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption"));
+            services.Configure<List<Client>>(Configuration.GetSection("Clients"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
